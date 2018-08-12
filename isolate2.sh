594cca9bd3f2825a3094732d0482b920127ec559
Richard Darst <rkd@zgib.net>
2018-08-12 13:12:41 +0300

    isolate2.sh: Refactoring, reduce verbosity, improvements

--- isolate2.sh
+++ isolate2.sh
@@ -1,50 +1,63 @@
 #!/bin/bash
 
-set -x
+VERBOSE=${VERBOSE:-}
+test -n "$VERBOSE" && set -x
 set -e
 
 METHOD=chroot
 
-# Phase 1: when first started.
-# Create the basic directory and unshare into it.
+# If VERBOSE is set, run the command
+debug () {
+    test -n "$VERBOSE" && eval "$@"
+    return 0
+}
+
+export NI_OLDID=${NI_OLDID:-`id -u`:0}
+
+# Phase 1: basic setup of the new namespace.
+# Create the base directory.
 if [ -z "$NI_PHASE" ] ; then
     MNTDIRS=${MNTDIRS:-.}
     # Things which should almost always be mounted
-    MNTDIRS_ALL=${MNTDIRS_ALL:-"/bin /lib /etc /usr /lib64 /proc"}
-    export MNTDIRS="$MNTDIRS_ALL $MNTDIRS"
+    MNTDIRS_BASE=${MNTDIRS_BASE:-"/bin /lib /etc /usr /lib64 /proc"}
+    export MNTDIRS="$MNTDIRS_BASE $MNTDIRS"
     # Make our tmpdir
     export NI_BASEDIR=`mktemp -d isolate.XXXXXXXX --tmpdir`
 
-    # Do the unshare
+    # Do the unshare.  Re-run this same script in phase 2.
     export NI_PHASE=2
-    unshare --user --map-root-user --mount-proc --pid --net --uts --fork \
-	    bash "$0" "$@"
+    unshare --map-root-user --mount-proc \
+	    --user --pid --net --uts --ipc --mount \
+	     --fork bash "$0" "$@"
 
     # Clean up: change to "trap" later after ensuring that there won't
     # be files left here and no chance of deleting stuff on mail
     # filesystem.
-    rm -d "$NI_BASEDIR"  # remote empty dir only...
+    # -d means 'remote empty dir only'
+    trap 'rm -d "$NI_BASEDIR"' EXIT KILL INT TERM
 
-# Phase 2: chroot into the tmpdir
+# Phase 2: Mount stuff and chroot into the tmpdir
 elif [ "$NI_PHASE" = 2 ] ; then
     echo "BEGIN phase 2"
-    whoami
+    debug whoami
+    # Mount a tmpfs to be our new root.
     mount -t tmpfs -o size=10k tmpfs "$NI_BASEDIR"
+    # Mount each dir in the basedir
     for dir in $MNTDIRS; do
 	dir=`realpath "$dir"`
 	mkdir -p "$NI_BASEDIR/$dir"
 	mount --bind "$dir" "$NI_BASEDIR/$dir"
     done
+    # Copy the isolate.sh script into the new base.
     #mount --bind "$0" "$NI_BASEDIR/isolate.sh"
     cp -p "$0" "$NI_BASEDIR/isolate.sh"
     mkdir -p "$NI_BASEDIR/tmp/"
 
+    debug whoami
+    debug ls -l "$NI_BASEDIR"
+
     export NI_PHASE=3
     export NI_OLDPWD=`realpath $PWD`
-
-    whoami
-    ls -l "$NI_BASEDIR"
-    ls -l "$NI_BASEDIR/m/home/home0/04/darstr1/unix/tmp/"
 
     # If chroot method
     if [ $METHOD = "chroot" ] ; then
@@ -55,17 +68,16 @@ elif [ "$NI_PHASE" = 2 ] ; then
 	true #...
     fi
 
-# Do any pre-setup in the dir
+# Do setup in the chroot.  change to our former pwd and run our command.
 elif [  "$NI_PHASE" = 3 ] ; then
     echo "BEGIN phase 3"
     cd "$NI_OLDPWD"
-    pwd
-    whoami
-    mount
+    debug mount
+    echo `whoami` in `pwd`
     echo "running command $@"
-    if [ -z "$@" ] ; then
-	exec bash
-    else
+    if [ "$#" -gt 0 ] ; then
 	eval "$@"
+    else
+	exec bash
     fi
 fi
